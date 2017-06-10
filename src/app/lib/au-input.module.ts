eb5328f048a40ea1cfdede83a7e6b65bfbaef1a8
Zhentian Wan <answer881215@gmail.com>
2017-06-10 21:12:06 +0300

    spreate theme and add altrivate theme

--- src/app/lib/au-input.module.ts
+++ src/app/lib/au-input.module.ts
@@ -1,17 +1,15 @@
-
-
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import {AuFaInputComponent} from "./au-fa-input/au-fa-input.component";
-import {InputRefDirective} from "./common/input-ref.directive";
-import { AuMdInputComponent } from './au-md-input/au-md-input.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {AuFaInputComponent} from './au-fa-input/au-fa-input.component';
+import {InputRefDirective} from './common/input-ref.directive';
 
 @NgModule({
-    declarations: [AuFaInputComponent, InputRefDirective, AuMdInputComponent],
-    imports: [
-        CommonModule
-    ],
-    exports: [AuFaInputComponent, AuMdInputComponent, InputRefDirective]
+  declarations: [AuFaInputComponent, InputRefDirective],
+  imports: [
+    CommonModule
+  ],
+  exports: [AuFaInputComponent, InputRefDirective]
 })
-export class AuInputModule { }
+export class AuInputModule {
+}
 
